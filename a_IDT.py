a4a9fa5095ee6a4eb19f76b768a762b997286407
thomasaref <ThomasAref@gmail.com>
2015-07-02 01:09:47 +0200

    adding updating to a_IDT

--- a_IDT.py
+++ a_IDT.py
@@ -5,17 +5,28 @@ Created on Thu Feb 26 11:08:19 2015
 @author: thomasaref
 """
 from a_Base import Base
-from atom.api import Enum, Int, Float, observe, Bool#, Property 
+from atom.api import Enum, Int, Float, observe, Bool, Property
 from scipy.constants import epsilon_0 as eps0
 from numpy import sqrt
 
+from functools import wraps
+
+def updater(fn):
+    @wraps(fn)
+    def myfunc(self, change):
+        if not self._update: # and change['type']!='create':
+            self._update=True
+            fn(self, change)
+            self._update=False
+    return myfunc
+
+
 class IDT(Base):
-    finger_type=Enum("double", "single").tag(desc="'double' for double fingered, 'single' for single fingered.",
+    ft=Enum("double", "single").tag(desc="'double' for double fingered, 'single' for single fingered.",
                                             mapping={"double" : 2, "single" : 1})
     Np=Int(7) #1, 1000, 7).tag(desc="number of finger pairs. this should be at least 1 and defaults to 36.")
     ef=Int()#0, 100, 0).tag(desc="number of extra fingers to compensate for edge effect. Defaults to 0")
     a=Float(0.05).tag(desc="width of fingers (um). same as gap generally.")
-    p=Float(0.1).tag(desc="periodicity. this should be twice width for 50% metallization")
     W=Float(25.0)#1, 500, 25.0).tag(desc="height of finger. Equivalenbt to W")
     g=Float(0.05).tag(desc="gap between fingers (um). about 0.096 for double fingers at 4.5 GHz")
     eta=Float(0.5).tag(desc="metalization ratio")
@@ -26,32 +37,58 @@ class IDT(Base):
     material = Enum('LiNbYZ', 'GaAs', 'LiNb128', 'LiNbYZX', 'STquartz')
 
     _update=Bool(False).tag(private=True)
-    
-    def do_update(self, name, new_val):
-        if not self._update:
-            self._update=True
-            setattr(self, name, new_val)
-            self._update=False
+
+    #@property
+    #def unit_map(self):
+    #    return dict(GHz=1.0e9)
+
+    @Property
+    def p(self):
+        return self.a+self.g
+    p.tag(desc="periodicity. this should be twice width for 50% metallization")
+    #def do_update(self, name, new_val):
+    #    if not self._update:
+    #        self._update=True
+    #        setattr(self, name, new_val)
+    #        self._update=False
 
     #def parse_equation(self, lhs="", rhs=[], eqn=""):
     #    eqn.replace(lhs, "self."+lhs)
     #    for el in rhs:
     #        eqn.
     #    for a in self.all_params:
-            
-    @observe('a', 'g')
-    def _update_eta(self, change):
-        a=self.a
-        g=self.g
-        eta=a/a+g
-        self.do_update("eta", eta)
-        
-    @observe('a', 'eta')
-    def update_p(self, change):
-        a=self.a
-        eta=self.eta
-        g=a*(1.0/eta-1.0)
-        self.do_update("g", g)
+
+    def _update_eta(self, a, g):
+        self.eta=a/(a+g)
+
+    def _update_g(self, a,eta):
+        self.g= a*(1.0/eta-1.0)
+
+    def _update_Ct(self, ft, W, epsinf, Np):
+        m={"double": sqrt(2.0), "single": 1.0}[ft]
+        self.Ct=m*(W*1.0e-6)*epsinf*Np
+
+    def _update_lbda0(self, v, f0):
+        self.lbda0=1.0e6*v/f0/1.0e9
+
+    def _update_a(self, ft, eta, lbda0):
+        m={"double": 2, "single": 1}[ft]
+        self.a=eta*lbda0/(2*m)
+
+    def _update_f0(self, v, lbda0):
+        self.f0=1.0e9*v/lbda0/1.0e6
+
+    @updater
+    def _observe_a(self, change):
+        self._update_eta(a=self.a, g=self.g)
+
+    @updater
+    def _observe_g(self, change):
+        self._update_eta(a=self.a, g=self.g)
+
+    @updater
+    def _observe_eta(self, change):
+        self._update_g(a=self.a, eta=self.eta)
 
     def _observe_material(self, change):
         if self.material=="STquartz":
@@ -77,43 +114,55 @@ class IDT(Base):
         else:
             print "Material not listed"
 
-    f0=Float().tag(label="f0",
+    f0=Float(5.0).tag(label="f0",
                 unit="GHz",
                 desc="Center frequency",
                 reference="")
 
-    @observe('a', 'v', 'p', 'double_finger')
-    def _update_f0(self, change):
-        v=self.v
-        p=self.p*1e-6
-        m=self.get_map("finger_type")
-        lbda0=m*2*p
-        f0=v/lbda0/1.0e9
-        self.do_update("f0", f0)
+    lbda0=Float(1.0).tag(label="lbda0",
+                unit="um",
+                desc="Center wavelength",
+                reference="")
+
+    @updater
+    def _observe_f0(self, change):
+        self._update_lbda0(v=self.v, f0=self.f0)
+        self._update_a(ft=self.ft, eta=self.eta, lbda0=self.lbda0)
+        self._update_g(a=self.a, eta=self.eta)
+
+    @updater
+    def _observe_v(self, change):
+        self._update_f0(v=self.v, lbda0=self.lbda0)
 
     Ct=Float().tag(label="Ct",
                    unit="F",
                    desc="Total capacitance of IDT",
                    reference="Morgan page 16/145")
 
-    @observe('epsinf', 'W', 'Np', 'df')
-    def _get_Ct(self, change):
-        W=self.W*1.0e-6 
-        epsinf=self.epsinf
-        Np=self.Np
-        if self.finger_type=="double":
-            Ct=sqrt(2.0)*W*epsinf*Np
-        else:
-            Ct=W*epsinf*Np
-        self.do_update("Ct", Ct)
+    @updater
+    def _observe_W(self, change):
+        self._update_Ct(ft=self.ft, W=self.W, epsinf=self.epsinf, Np=self.Np)
+
+    @updater
+    def _observe_ft(self, change):
+        self._update_Ct(ft=self.ft, W=self.W, epsinf=self.epsinf, Np=self.Np)
+
+    @updater
+    def _observe_epsinf(self, change):
+        self._update_Ct(ft=self.ft, W=self.W, epsinf=self.epsinf, Np=self.Np)
+
+    @updater
+    def _observe_Np(self, change):
+        self._update_Ct(ft=self.ft, W=self.W, epsinf=self.epsinf, Np=self.Np)
 
     def _default_material(self):
         return 'LiNbYZ' #'GaAs'
 
 if __name__=="__main__":
     a=IDT()
+    print a._observe_a, a._observe_g#, a.update_p
     a.show()
-    
+
 #from scipy.constants import epsilon_0 as eps0
 #from numpy import sqrt
 
@@ -185,4 +234,4 @@ if __name__=="__main__":
 #        return W*epsinf*Np
 #
 #    def _default_material(self):
-#        return 'LiNbYZ' #'GaAs'    
+#        return 'LiNbYZ' #'GaAs'
