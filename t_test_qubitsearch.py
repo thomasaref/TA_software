df6491e97068eaf9dae57b43ac1630098f6a4923
thomasaref <ThomasAref@gmail.com>
2015-09-10 18:16:25 +0200

    a_Plotter start

--- t_test_qubitsearch.py
+++ t_test_qubitsearch.py
@@ -6,7 +6,7 @@ Created on Wed Aug 19 10:47:37 2015
 """
 
 from Atom_Read_File import Read_HDF5
-from numpy import squeeze, shape, linspace, mean, transpose
+from numpy import squeeze, shape, linspace, mean, transpose, log10
 #from Plotter import Plotter
 #a=Read_HDF5(file_path="/Users/thomasaref/Dropbox/Current stuff/Logbook/T_testy16.hdf5")
 #a=Read_HDF5(file_path="/Users/thomasaref/Dropbox/Current stuff/Logbook/TA_D_characterize10_2p6kOhm.hdf5")
@@ -23,8 +23,9 @@ a.read()
 Magvec=a.data["Traces"]["Agilent VNA - S21"].data
 print shape(Magvec)
 from numpy import absolute
-Magvec=absolute(Magvec[:,0,:]+1j*Magvec[:,1,:])
+Magvec=20*log10(absolute(Magvec[:,0,:]+1j*Magvec[:,1,:]))
 #Magvec=transpose(Magvec)-Magvec[:, 500] #mean(Magvec, axis=1, keepdims=True)
+#Magvec=transpose(transpose(Magvec)-Magvec[:, 0])
 
 yoko=a.data["Data"]["Data"].data
 yoko=squeeze(yoko)
@@ -39,23 +40,66 @@ b.read()
 
 Magvecb=b.data["Traces"]["Agilent VNA - S21"].data
 print shape(Magvecb)
-Magvecb=absolute(Magvecb[:,0,:]+1j*Magvecb[:,1,:])
+Magvecb=20*log10(absolute(Magvecb[:,0,:]+1j*Magvecb[:,1,:]))-20
 
 f0, fstep=squeeze(b.data["Traces"]['Agilent VNA - S21_t0dt'].data)
 l=shape(Magvecb)[0]
 freqb=linspace(f0, f0+fstep*(l-1), l)
 
+
 #a=Plotter()
-from numpy import array
-from matplotlib.collections import LineCollection
+#from numpy import array
 #a.axe.plot(freq, Magvec[:,1])
 #a.axe.imshow(Magvec)
 #b=LineCollection(((yoko, Magvec[1,:]),))
 #a.show()
 
 import matplotlib.pyplot as plt
-plt.plot(freq, Magvec[:,1])
-plt.plot(freqb, Magvecb[:,1])
+#plt.plot(freq, Magvec[:,1])
+#plt.plot(freqb, Magvecb[:,1])
 
-#plt.imshow(Magvec)
+#ax=plt.imshow(Magvec, aspect="auto", extent=[min(yoko),max(yoko), min(freq),max(freq)])#, interpolation="sinc") 
+#ax.axes.set_title("blah")
+#ax.axes.set_xlabel("yoko")
+#ax.axes.set_ylabel("freq")
+
+
+from matplotlib.collections import LineCollection
+from matplotlib.colors import colorConverter
+# Make a list of colors cycling through the rgbcmyk series.
+colors = [colorConverter.to_rgba(c) for c in ('r','g','b','c','y','m','k')]
+
+fig, ax4 = plt.subplots(1,1)
+#nverts = 60
+#ncurves = 20
+#offs = (0.1, 0.0)
+#
+#from numpy import amax, cos, random, pi
+#rs = random.RandomState([12345678])
+#
+#yy = linspace(0, 2*pi, nverts)
+#ym = amax(yy)
+#xx = (0.2 + (ym-yy)/ym)**2 * cos(yy-0.4) * 0.5
+#segs = []
+#for i in range(ncurves):
+#    xxx = xx + 0.02*rs.randn(nverts)
+#    curve = list(zip(xxx, yy*100))
+#    segs.append(curve)
+#    if i<2:
+#        print segs
+
+segs=[]
+segs.append(list(zip(freq, Magvec[:, 1])))
+segs.append(list(zip(freqb, Magvecb[:, 1])))
+
+col = LineCollection(segs)#, offsets=offs)
+ax4.add_collection(col, autolim=True)
+col.set_color(colors)
+ax4.autoscale_view()
+ax4.set_title('Successive data offsets')
+ax4.set_xlabel('Zonal velocity component (m/s)')
+#ax4.set_ylabel('Depth (m)')
+# Reverse the y-axis so depth increases downward
+#ax4.set_ylim(ax4.get_ylim()[::-1])
+
 plt.show()
