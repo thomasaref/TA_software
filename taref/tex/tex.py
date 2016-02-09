7dbef05f85431936db83c1954c68e473aa7acbd5
thomasaref <ThomasAref@gmail.com>
2016-02-09 19:28:05 +0100

    work on tex

--- taref/tex/tex.py
+++ taref/tex/tex.py
@@ -8,21 +8,34 @@ Created on Fri Oct  9 17:17:26 2015
 from atom.api import Atom, Unicode, Typed, List, cached_property
 from taref.tex.tex_backbone import (texwrap, include_figure, extract_block, make_table,
                                      mult_fig_start, mult_fig_end, add_mult_fig, include_image, compile_tex)
+
+from collections import OrderedDict
+
 from taref.core.agent import SubAgent as Operative
 from enaml import imports
 with imports():
     from tex_e import TEX_Window
 
-
 def line_check(line):
     inline=line.strip()
     if inline.startswith("\pyb"):
         line_check.inblock=True
-        return False
+        line_check.key=line.split("\pyb")[1].split("{")[1].split("}")[0].strip()
+        #return False
     elif inline.startswith("\pye"):
         line_check.inblock=False
-    return line_check.inblock
+        return True
+    if line_check.inblock:
+        tlist=line_check.dict.get(line_check.key, [])
+        tlist.append(line)
+        line_check.dict[line_check.key]=tlist
+    return False #line_check.inblock
 line_check.inblock=False
+line_check.key=""
+line_check.dict={}
+
+from itertools import izip
+
 
 class TEX(Operative):
 
@@ -30,14 +43,54 @@ class TEX(Operative):
     #write_file=Typed(Write_File)
 
     tex_list=List()
-    input_tex=Unicode().tag(spec="multiline", no_spacer=True)
-    output_tex=Unicode().tag(spec="multiline", no_spacer=True)
+    input_code=Unicode()
+    input_tex=Unicode()
+    input_tex_list=List()
+    output_tex=Unicode()
+    source_tex=List()
 
+    source_dict=Typed(OrderedDict, ())
+
+    index_list=List()
+    tlist=List()
+    def make_input_tex(self):
+        self.index_list=[]
+        self.tlist=[]
+        for line in self.input_code.split("\n"):
+            line=line.strip()
+            key=None
+            if line.startswith("tx.ext("):
+                try:
+                    key=line.split("tx.ext('")[1].split("')")[0]
+                except IndexError:
+                    key=line.split('tx.ext("')[1].split('")')[0]
+            self.tlist.append(line)
+            self.index_list.append(None)
+            if key is not None:
+                source_list=zip(*[(key, "#"+item) for item in self.source_dict[key]])
+                self.index_list.extend(source_list[0])
+                self.tlist.extend(source_list[1])
+        self.input_tex="\n".join(self.tlist)
 
     def _observe_input_tex(self, change):
-        inlist=self.input_tex.split("\n")
-        outlist=[line for line in inlist if line_check(line)]
-        self.output_tex="\n".join(outlist)
+
+        if change["type"]=="update":
+            #self.make_input_tex()
+            lineno=[n for n, (line1, line2) in enumerate(izip(change["oldvalue"].split("\n"), self.input_tex.split("\n"))) if line1 != line2][0]
+            print lineno, self.index_list[lineno]
+            key=self.index_list[lineno]
+            if key is not None:
+                #source_dict[key]=
+                print [line for n, line in enumerate(self.tlist) if self.index_list[n]==key]
+
+    def process_source_tex(self):
+        for n, line in enumerate(self.source_tex):
+            line=line.strip()
+            if line.startswith("\pyb"):
+                index=n+1
+                key=line.split("\pyb")[1].split("{")[1].split("}")[0].strip()
+            elif line.startswith("\pye"):
+                self.source_dict[key]=["# "+item for item in self.source_tex[index:n]]
 
 
 #    """wrapper object which combines tex functions"""
@@ -62,11 +115,10 @@ class TEX(Operative):
         self.tex_list.append(inline)
 
     def extract_block(self, name):
-        return extract_block(name, self.input_tex.split("\n"))
+        return extract_block(name, self.source_tex)
 
     def ext(self, block_name):
         temp_list=self.extract_block(block_name)
-        print temp_list
         self.extend(temp_list)
 
     def extend(self, inlist):
@@ -100,14 +152,22 @@ if __name__=="__main__":
 
     #print extract_block("bob", str_list)
 
-    tx.input_tex="""\pyb {summary}
+    tx.source_tex="""\pyb {summary}
     \section{Summary}
     This is an attempt at reducing the coupling by changing the spacing of the fingers pairs on the qubit IDT, pushing them to a higher frequency. The coupling appears reduced by an order of magnitude and the qubit seems to be operating as a qubit. Unfortunately, the qubit frequency is below the IDT listening/talking frequency so it is never directly on resonance (this could be easily fixed by having less resistive junctions). Speedy was also experiencing quite a bit of trouble with blockages so the temperature was often in the 50-80 mK range.
 
-\pye"""
+\pye
 
-    print tx.output_tex
+\pyb {second entry}
+this is the second entry
+\pye""".split("\n")
 
+    tx.input_code="""tx.ext("summary")
+
+    tx.ext("second entry")"""
+    tx.process_source_tex()
+    tx.make_input_tex()
+    print tx.source_dict
     if 1:
         qubit_values=[[r"Qubit"                                  ,  r"{}"                             ],
                       [r"Finger type"                            ,  r"double finger"                  ],
