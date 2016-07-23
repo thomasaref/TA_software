e1b16676c695474b59dab385bb888a48f6eb60d1
thomasaref <ThomasAref@gmail.com>
2016-07-23 11:55:16 +0200

    more test atom

--- taref/test/test_atom.py
+++ taref/test/test_atom.py
@@ -5,44 +5,137 @@ Created on Fri Jul 22 14:51:44 2016
 @author: thomasaref
 """
 
-from atom.api import Atom, Float
+from atom.api import Atom, Float, cached_property
 from collections import OrderedDict
+#from enaml import imports
+#from enaml.core.operators import op_subscribe
+#def testy(*args, **kwargs):
+#    print args[0], args[1], kwargs
+#    print dir(args[0])
+#    return op_subscribe(*args, **kwargs)    
+#with imports(operators={"<<" : testy}, union=True):
+#    from taref.test.test_atom_e import Main
 
-#class Test(Atom):
-#    a=Float().tag(blue="good")
+from PyQt4 import QtGui
+import sys
 
+class Example(QtGui.QWidget):
+    
+    def __init__(self, obj):
+        super(Example, self).__init__()
+        
+        self.initUI()
+        self.obj=obj
+
+    def onClicked(self):
+        print "clicked"
+        print dir(self.titleEdit)
+        #self.obj.c=int(self.titleEdit.text())
+        self.titleEdit.setText(str(self.obj.blah))
+    def txchng(self):
+        print "text changed"
+        self.obj.c=int(self.titleEdit.text())
+        
+        
+    def initUI(self):
+        
+        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
+        
+        self.setToolTip('This is a <b>QWidget</b> widget')
+        
+        btn = QtGui.QPushButton('Button', self)
+        print dir(btn)
+        btn.setText("yay")
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)       
+        btn.clicked.connect(self.onClicked)
+        #self.connect(btn, QtGui.QtCore.SIGNAL('clicked()'), self.onClicked)
+        self.titleEdit = QtGui.QLineEdit()
+        self.titleEdit.textChanged.connect(self.txchng)
+        grid = QtGui.QGridLayout()
+        grid.setSpacing(10)
+
+        grid.addWidget(self.titleEdit, 1, 0)
+        
+        grid.addWidget(btn, 1, 1)
+        self.setLayout(grid) 
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Tooltips')    
+        self.show()
+        
+def main():
+    
+    app = QtGui.QApplication(sys.argv)
+
+    w = Example(obj=b)
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+    
+    sys.exit(app.exec_())
+
+
+ind=100000
+class Test(Atom):
+    a=Float().tag(blue="good")
+
+    @cached_property
+    def b(self):
+        return range(ind)
+        
+#print dir(Atom)
+
+#print dir(object)
 class Value(object):
+    typer=None
+    coercer=None
+    
     def tag(self, **kwargs):
         self.metadata.update(kwargs)
         return self
+        
+    def __init__(self):
+        self.metadata={}
 
 class Property(Value):
-    def __init__(self, fget=None, fset=None, cached=True):
+    def __init__(self, fget=None, fset=None, fdel=None, cached=True):
+        super(Property, self).__init__()
         self.fget=fget
         self.fset=fset
+        self.fdel=fdel
         self.cached=cached
-        self.metadata={"typer":int}
+        self.do_reset=True
+        
+    def reset(self):
+        self.do_reset=True
 
     def __get__(self, obj, typ):
-        print self, obj, typ
-        return self.fget(self)
+        if self.do_reset:
+            self.value=self.fget(obj)
+            self.do_reset=not self.cached
+        return self.value
 
 class Int(Value):
     coercer=int
-    def __init__(self, value=0):
-        self.value=value
+    def __init__(self, default=0):
+        self.default=default
         self.metadata={}
 
-
     def __get__(self, obj, typ=None):
-        print "got things"
-        return self.value
+        return obj.__slots__.get(self, self.default)
 
     def __set__(self, obj, value):
         obj.notify(self, value)
-        self.value=self.coercer(value)
+        obj.__slots__[self]=self.coercer(value)
+
 
 class myAtom(object):
+    def __init__(self, **kwargs):
+        self.__slots__={}
+        for kw in kwargs:
+            self.__slots__[self.get_member(kw)]=kwargs[kw]
 
     @classmethod
     def get_member(cls, name):
@@ -50,7 +143,7 @@ class myAtom(object):
 
     @classmethod
     def members(cls):
-        return dict([(name, member) for name, member in cls.__dict__.items() if hasattr(member, "tag")])
+        return dict([(name, member) for name, member in cls.__dict__.items() if isinstance(member, Value)])
 
     def set_tag(self, name, **kwargs):
         self.get_member(name).metadata.update(kwargs)
@@ -58,18 +151,35 @@ class myAtom(object):
     def get_tag(self, name, tag, none_value=None):
         return self.get_member(name).metadata.get(tag, none_value)
 
-
-
 class test(myAtom):
-    b=3
     c=Int().tag(five=5, typer=int)
 
+    #def __init__(self, **kwargs):
+    #    super(test, self).__init__(**kwargs)
+
+    def _observe_c(self, change):
+        print change
+        self.get_member("blah").reset()
+        
     def notify(self, *args):
         print "parent", args
+        #self.get_member("blah").reset()
 
     @Property
     def blah(self):
-        return 2
+        return 4 #range(ind)
+
+    @Property
+    def d(self):
+        return range(ind)
+        
+    @property
+    def red(self):
+        return range(ind)
+        
+    @property
+    def view_window(self):
+        return Main(test=self)
 
 #    def __init__(self, a=2):
 #        #self.a=a
@@ -79,27 +189,57 @@ class test(myAtom):
 
 
 
+if __name__ == '__main__':
+    from time import time
+    a=Test()
+    b=test(c=10)
+    c=test()
+    
+    print b.__dict__
+    print b.c, c.c
+    c.c=20
+    print b.c, c.c
+    print dir(b)
 
-#a=Test()
-b=test()
-print b.c
-print b.blah
-b.c=4
-print b.c
-print b.get_member("c")
-print b.get_member("c").metadata
-print b.members()
-b.set_tag("c", g=2)
-print b.get_tag("c", "g")
-print b.get_tag("c", "g2")
-print b.get_tag("c", "five")
+    #tstart=time()
+    #for n in range(1000):
+    #    b.red
+    #print tstart-time()
 
-from taref.core.shower import shower
-from taref.core.api import get_main_params, get_all_tags
-print get_main_params(b)
-print get_all_tags(b, key="private", key_value=False, none_value=False)
-print b.members()
-shower(b)
+    
+
+    
+    
+    tstart=time()
+    for n in range(100000):
+        a.b
+    print tstart-time()
+
+    tstart=time()
+    for n in range(100000):
+        b.d
+    print tstart-time()
+
+    #main()
+if 0:
+    print b.c
+    print b.blah
+    b.c=4
+    print b.c
+    print b.get_member("c")
+    print b.get_member("c").metadata
+    print b.members()
+    b.set_tag("c", g=2)
+    print b.get_tag("c", "g")
+    print b.get_tag("c", "g2")
+    print b.get_tag("c", "five")
+    
+    from taref.core.shower import shower
+    from taref.core.api import get_main_params, get_all_tags
+    print get_main_params(b)
+    print get_all_tags(b, key="private", key_value=False, none_value=False)
+    print b.members()
+#shower(b)
 
 #print dir(a)
 #print dir(b)
---
+++ taref/test/test_atom2.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 23 10:31:25 2016
+
+@author: thomasaref
+"""
+class Value(object):
+    __slots__=["value", "metadata", "typer"]
+    def __init__(self, value):
+        self.value=value
+        self.metadata={}
+        if value is None:
+            self.typer=None
+        else:            
+            self.typer=type(value)
+        
+    def tag(self, **kwargs):
+        self.metadata.update(kwargs)
+        return self
+
+
+def test_f(obj):
+    print obj
+
+def test_f2():
+    print "hi"
+    
+class Big(object):
+    def get_tag(self, name, key, none_value=None):
+        member=self.get_member(name)
+        if member is None:
+            return none_value
+        return member.metadata.get(key, none_value)
+        
+    def set_tag(self, name, **kwargs):
+        self.__members__[name].tag(**kwargs)
+        
+    def get_member(self, name):
+        return self.__members__.get(name, None)
+
+    def notify(self, name, value):
+        print name, value
+        
+    def set_attr(self, name, value):
+        setattr(self, name, value)
+        if name in self.__members__:
+            return self.get_member(name)
+        
+    def __setattr__(self, name, value):
+        if not name.startswith("_") and not name.endswith("_"):
+            if not hasattr(self, "__members__"):
+                self.__members__={}
+            cval=self.__members__.pop(name, None)
+            if cval is None:
+                cval=Value(value)
+            else:
+                if type(value)!=cval.typer:
+                    raise Exception("Wrong type")
+                cval.value=value
+            self.__members__[name]=cval
+            self.notify(name, value)
+        super(Big, self).__setattr__(name, value)
+            
+    def __getattribute__(self, name):
+        cval=None
+        if not name.startswith("_") and not name.endswith("_"):
+            cval=self.__members__.get(name, None)
+        if cval is None:  
+            return super(Big, self).__getattribute__(name)
+        return cval.value
+           
+class Test(Big):
+    def __init__(self, a=1):
+        self.a=a
+        self._b=5
+        self.set_tag("a", private=True)
+
+if __name__=="__main__":
+    a=Test()
+    a.set_attr("d", 5.0).tag(g=3)
+    print a.a, a._b
+    print a.get_tag("a", "private")
+    print a.get_tag("d", "g")
+    print a.__members__
+    print a.get_member("a").typer
+    a.a=10
+    print a.a, a._b
+    a._b="1"  
+    a.b_="1"
+    a.c=test_f
+    a.c(3)
+    print a.__members__
+    a.c=test_f2
+    a.c()
+    a.c="d"
